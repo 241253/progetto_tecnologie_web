bd5d7e51592fa1ba3e698cc218054c41f3c7dd37
241253 <241253@studenti.unimore.it>
2020-05-14 17:10:13 +0200

    J - 'home prova'

--- progetto_tecnologie_web/urls.py
+++ progetto_tecnologie_web/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.landingPage, name='home'),
     path('admin/', admin.site.urls),
 ]
