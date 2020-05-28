7065cc8d4017a01c4e8699b2bfe63665af7230b5
241253 <241253@studenti.unimore.it>
2020-05-28 10:28:42 +0200

    J: Home

--- progetto_tecnologie_web/views.py
+++ progetto_tecnologie_web/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 
 
+@never_cache
 def landingPage(request):
     return render(request, 'home.html')
