9f386540cdf0ae49ba6183bef490b64f125e13be
michele <243582@studenti.unimore.it>
2020-06-17 17:04:55 +0200

    Z: nuovo commit

--- user_management/admin.py
+++ user_management/admin.py
@@ -8,7 +8,7 @@ from django.contrib.auth.admin import UserAdmin
 class MyUserAdmin(UserAdmin):
     list_display = ("email", "nome", "cognome", "last_login", "is_admin", "is_staff", "is_active")
     search_fields = ("email", "nome", "cognome")
-    readonly_fields = ("last_login")
+    readonly_fields = ("last_login",)
 
     filter_horizontal = ()
     list_filter = ()
