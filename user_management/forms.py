387becf4ff5b538fa25ee71f72b2a9507c68de73
michele <243582@studenti.unimore.it>
2020-06-24 16:08:03 +0200

    Z: ti meriti un po' di lavoro

--- user_management/forms.py
+++ user_management/forms.py
@@ -2,6 +2,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
 from django.contrib.auth.models import User
+from django.core.files.images import get_image_dimensions
 
 from user_management.models import Profile
 
@@ -15,3 +16,36 @@ class ProfileCreationForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('foto',)
+
+    def clean_avatar(self):
+        foto = self.cleaned_data['foto']
+
+        try:
+            w, h = get_image_dimensions(foto)
+
+            #validate dimensions
+            max_width = max_height = 1000
+            if w > max_width or h > max_height:
+                raise forms.ValidationError(
+                    u'Please use an image that is '
+                     '%s x %s pixels or smaller.' % (max_width, max_height))
+
+            #validate content type
+            main, sub = foto.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG, '
+                    'GIF or PNG image.')
+
+            #validate file size
+            if len(foto) > (4000 * 1024):
+                raise forms.ValidationError(
+                    u'Avatar file size may not exceed 4MB.')
+
+        except AttributeError:
+            """
+            Handles case when we are updating the user profile
+            and do not supply a new avatar
+            """
+            pass
+
+        return foto
--- user_management/models.py
+++ user_management/models.py
@@ -1,12 +1,14 @@
+from django.core.files.images import get_image_dimensions
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.forms import forms
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    foto = models.ImageField(upload_to='user_db/user_photo') #foto profilo dell'utente
+    foto = models.ImageField(upload_to='user_db/user_photo', default='user_db/None/none_picture.png') #foto profilo dell'utente
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
