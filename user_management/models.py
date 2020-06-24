23100cc9f47156a1f1f4de3d0923ec36b4c6cd5b
241253 <241253@studenti.unimore.it>
2020-06-24 15:52:38 +0200

    J - utente versione 2

--- user_management/models.py
+++ user_management/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    foto = models.FileField(upload_to='user_db/user_photo') #foto profilo dell'utente
+    foto = models.ImageField(upload_to='user_db/user_photo') #foto profilo dell'utente
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
