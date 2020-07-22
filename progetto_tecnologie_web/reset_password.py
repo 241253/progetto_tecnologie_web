09599c10d10b0da5bef1180fbc273bd3352f0db0
241253 <241253@studenti.unimore.it>
2020-07-22 15:59:15 +0200

    J - pagina utente

---
+++ progetto_tecnologie_web/reset_password.py
@@ -0,0 +1,8 @@
+#RESET PASSWORD
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'virgiliancodeonline@gmail.com'
+EMAIL_HOST_PASSWORD = '243582241253'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'virgiliancodeonline@gmail.com'
